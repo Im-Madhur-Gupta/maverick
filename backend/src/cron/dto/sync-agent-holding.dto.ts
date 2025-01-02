b77865921866baf3e1ff3bd6b727d89b249f2056
Madhur Gupta <madhurgupta107@gmail.com>
2025-01-02 14:51:01 +0530

    feat(backend): add validation decorators in SyncAgentHoldingsDto

--- backend/src/cron/dto/sync-agent-holding.dto.ts
+++ backend/src/cron/dto/sync-agent-holding.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class SyncAgentHoldingsDto {
+  @IsString()
+  @IsNotEmpty()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   externalId: string;
 }
